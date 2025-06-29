9e9e33ca68759805e308764dd2504bf681d86dfc
Mohamed Gamal <mohamed.g.shoaib@gmail.com>
2025-06-29 14:29:42 +0300

    fix: add bookmark_id to type Bookmark

--- src/Bookmark/models/bookmark.model.ts
+++ src/Bookmark/models/bookmark.model.ts
@@ -1,6 +1,7 @@
 import { Connection, Document, Schema } from 'mongoose';
 
 export type Bookmark = {
+  bookmark_id?: string;
   url: string;
   title: string;
   description?: string;
@@ -12,7 +13,7 @@ export type BookmarkDocument = Bookmark & Document;
 
 export const bookmarkSchema = new Schema<BookmarkDocument>(
   {
-    // id: { type: String, required: true }, Mistake: id is auto generated
+    bookmark_id: { type: String, required: true },
     url: { type: String, required: true },
     title: { type: String, required: true },
     description: { type: String },

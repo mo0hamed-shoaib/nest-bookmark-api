889933a397e03ba054748e96f2b3dd3d9d600e8b
Mohamed Gamal <mohamed.g.shoaib@gmail.com>
2025-06-30 20:25:02 +0300

    chore(app): wire up modules and global configuration in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,9 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongoModule } from './Mongo/mongo.module';
 import { BookmarkModule } from './Bookmark/bookmark.module';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
@@ -9,8 +12,11 @@ import { BookmarkModule } from './Bookmark/bookmark.module';
       isGlobal: true,
       envFilePath: '.env',
     }),
+    MongooseModule.forRoot(process.env.MONGO_URI || 'mongodb://localhost/bookmark-api'),
     MongoModule,
     BookmarkModule,
+    AuthModule,
+    UserModule,
   ],
   controllers: [],
   providers: [],
